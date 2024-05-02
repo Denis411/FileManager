97940c532ae36a3bfa93de0c645839c60e1144b6
FIX PRICE <RETAIL@Deniss-MacBook-Pro.local>
2024-05-02 10:28:14 +0700

    Add VStack

--- FileManagerPackage/ContentView.swift
+++ FileManagerPackage/ContentView.swift
@@ -4,52 +4,54 @@ struct ContentView: View {
     @StateObject var viewModel = ViewModel()
     
     var body: some View {
-        VStack(spacing: 5){
-            Text(viewModel.actionDescription)
-            
-            Group {
-                TextField(.init("Enter text to save"), text: $viewModel.text)
-                TextField(.init("File name without extension"), text: $viewModel.fileNameWithoutExtension)
-            }
-            .padding()
-            .background(Color.green)
-            .cornerRadius(10)
-            .padding(.horizontal, 10)
-            
-            Toggle(isOn: $viewModel.shouldOverwriteExistingFile) {
-                Text("Should overwrite existing file")
-                    .font(.system(size: 10))
-            }
-            .toggleStyle(.switch)
-            .padding(.horizontal)
-            .padding(.vertical, 5)
-            .background(Color.gray.opacity(0.5))
-            .cornerRadius(10)
-            .padding(.horizontal, 10)
-            
-            Group {
-                Button {
-                    viewModel.saveData()
-                } label: {
-                    Text("Save data")
-                }
+        ScrollView {
+            VStack(spacing: 5){
+                Text(viewModel.actionDescription)
                 
-                Button {
-                    viewModel.resaveData()
-                } label: {
-                    Text("Resave data")
+                Group {
+                    TextField(.init("Enter text to save"), text: $viewModel.text)
+                    TextField(.init("File name without extension"), text: $viewModel.fileNameWithoutExtension)
                 }
+                .padding()
+                .background(Color.green)
+                .cornerRadius(10)
+                .padding(.horizontal, 10)
                 
-                Button {
-                    viewModel.deleteData()
-                } label: {
-                    Text("Delete data")
+                Toggle(isOn: $viewModel.shouldOverwriteExistingFile) {
+                    Text("Should overwrite existing file")
+                        .font(.system(size: 10))
                 }
+                .toggleStyle(.switch)
+                .padding(.horizontal)
+                .padding(.vertical, 5)
+                .background(Color.gray.opacity(0.5))
+                .cornerRadius(10)
+                .padding(.horizontal, 10)
+                
+                Group {
+                    Button {
+                        viewModel.saveData()
+                    } label: {
+                        Text("Save data")
+                    }
+                    
+                    Button {
+                        viewModel.resaveData()
+                    } label: {
+                        Text("Resave data")
+                    }
+                    
+                    Button {
+                        viewModel.deleteData()
+                    } label: {
+                        Text("Delete data")
+                    }
+                }
+                .foregroundColor(.white)
+                .padding()
+                .background(Color.blue)
+                .cornerRadius(5)
             }
-            .foregroundColor(.white)
-            .padding()
-            .background(Color.blue)
-            .cornerRadius(5)
         }
     }
 }
