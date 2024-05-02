89850a410d0b0b9fbab449adee19d0d0a47e6e07
FIX PRICE <RETAIL@Deniss-MacBook-Pro.local>
2024-05-02 17:42:37 +0700

    Add test case
    Test saving valid data to disc

--- FileManagerSPM/Tests/FileManagerSPMTests/FileManagerSPMTests.swift
+++ FileManagerSPM/Tests/FileManagerSPMTests/FileManagerSPMTests.swift
@@ -2,11 +2,79 @@ import XCTest
 @testable import FileManagerSPM
 
 final class FileManagerSPMTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
+    private let data = "This is data for testing, it should be deleted".data(using: .utf8)!
+    private let fileName = "TestFile"
+    private let fileExtension = "json"
+    
+    private let package = FileManagerSPM()
+    
+    override func setUpWithError() throws {
+        do {
+            try self.removeFile(with: fileName, with: fileExtension)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func testSavingWithValidData() async {
+        // give
+        let shouldOverwriteFile = false
+        // when
+        do {
+            try await package.saveInAppDirectory(
+                data: data,
+                fileName: fileName,
+                fileExtension: fileExtension,
+                shouldOverwriteFile: shouldOverwriteFile
+            )
+        } catch {
+            // then 1
+            print(error.localizedDescription)
+            XCTFail()
+        }
+        
+        do {
+            let hasFileBeenSaved = try doesFileExist(with: fileName, with: fileExtension)
+            if !hasFileBeenSaved {
+                // then 2
+                print(#line.description, "file has not beed persisted")
+                XCTFail()
+            }
+        } catch {
+            // then 3
+            print(error.localizedDescription)
+            XCTFail()
+        }
+    }
+}
 
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+extension FileManagerSPMTests {
+    private func getAppDirectory() throws -> URL {
+        try FileManager.default.url(
+            for: .documentDirectory,
+            in: .userDomainMask,
+            appropriateFor: nil,
+            create: false
+        )
+    }
+    
+    private func doesFileExist(with fileName: String, with fileExtension: String) throws -> Bool {
+        let appDirectory = try getAppDirectory()
+        
+        let fileURL = appDirectory
+            .appendingPathComponent(fileName)
+            .appendingPathExtension(fileExtension)
+        
+        return FileManager.default.fileExists(atPath: fileURL.path)
+    }
+    
+    private func removeFile(with fileName: String, with fileExtension: String) throws {
+        let appDirectory = try getAppDirectory()
+        
+        let fileURL = appDirectory
+            .appendingPathComponent(fileName)
+            .appendingPathExtension(fileExtension)
+        
+        try FileManager.default.removeItem(at: fileURL)
     }
 }
