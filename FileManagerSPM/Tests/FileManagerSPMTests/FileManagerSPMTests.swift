526ae34da5ec062596723b097391fd47b93ebda3
FIX PRICE <RETAIL@Deniss-MacBook-Pro.local>
2024-05-06 12:26:00 +0700

    Correct tests

--- FileManagerSPM/Tests/FileManagerSPMTests/FileManagerSPMTests.swift
+++ FileManagerSPM/Tests/FileManagerSPMTests/FileManagerSPMTests.swift
@@ -1,60 +1,51 @@
 import XCTest
 @testable import FileManagerSPM
 
-final class FileManagerSPMTests: XCTestCase {
+final class FPFileManagerTests: XCTestCase {
     private let data = "This is data for testing, it should be deleted".data(using: .utf8)!
     private let fileName = "TestFile"
     private let fileExtension = "json"
-    
-    private let package = FileManagerSPM()
-    
-    override func setUpWithError() throws {
+
+    private let fileManager = FileManagerSPM()
+
+    override func tearDown() {
         do {
-            try self.removeFile(with: fileName, with: fileExtension)
+            guard let doesFileExist = try? doesFileExist(with: fileName, with: fileExtension),
+                  doesFileExist == true else {
+                return
+            }
+            try removeFile(with: fileName, with: fileExtension)
         } catch {
-            print(error.localizedDescription)
+            XCTFail(error.localizedDescription)
         }
     }
-    
+
     func testSavingWithValidData() async {
         // give
         let shouldOverwriteFile = false
         // when
         do {
-            try await package.saveInAppDirectory(
+            try await fileManager.saveInAppDirectory(
                 data: data,
                 fileName: fileName,
                 fileExtension: fileExtension,
                 shouldOverwriteFile: shouldOverwriteFile
             )
-            // then success
+            // then
+            let doesFileExist = try? doesFileExist(with: fileName, with: fileExtension)
+            XCTAssertTrue(doesFileExist ?? false)
         } catch {
-            // then 1
-            print(error.localizedDescription)
-            XCTFail()
-        }
-        
-        do {
-            let hasFileBeenSaved = try doesFileExist(with: fileName, with: fileExtension)
-            if !hasFileBeenSaved {
-                // then 2
-                print(#line.description, "file has not beed persisted")
-                XCTFail()
-            }
-        } catch {
-            // then 3
-            print(error.localizedDescription)
-            XCTFail()
+            XCTFail(error.localizedDescription)
         }
     }
-    
+
     func testSavingWithInvalidExtension() async {
         // give
         let shouldOverwriteFile = false
         let invalidFileExtension = ".json"
         // when
         do {
-            try await package.saveInAppDirectory(
+            try await fileManager.saveInAppDirectory(
                 data: data,
                 fileName: fileName,
                 fileExtension: invalidFileExtension,
@@ -62,118 +53,112 @@ final class FileManagerSPMTests: XCTestCase {
             )
         } catch {
             // then
-            guard error as? FileManagerErrors == FileManagerErrors.fileExtensionFirstCharacterIsDot else {
-                XCTFail()
-                return
-            }
+            let doesFileExist = try? doesFileExist(with: fileName, with: fileExtension)
+            XCTAssertFalse(doesFileExist ?? true)
+
+            XCTAssertEqual(error as? FileManagerError, FileManagerError.fileExtensionFirstCharacterIsDot)
         }
     }
-    
+
     func testSavingWithoutName() async {
         // give
         let shouldOverwriteFile = false
         let emptyFileName = ""
-        let invalidFileExtension = "json"
         // when
         do {
-            try await package.saveInAppDirectory(
+            try await fileManager.saveInAppDirectory(
                 data: data,
                 fileName: emptyFileName,
-                fileExtension: invalidFileExtension,
+                fileExtension: fileExtension,
                 shouldOverwriteFile: shouldOverwriteFile
             )
         } catch {
             // then
-            guard error as? FileManagerErrors == FileManagerErrors.fileWithoutName else {
-                XCTFail()
-                return
-            }
+            let doesFileExist = try? doesFileExist(with: fileName, with: fileExtension)
+            XCTAssertFalse(doesFileExist ?? true)
+
+            XCTAssertEqual(error as? FileManagerError, FileManagerError.fileWithoutName)
         }
     }
-    
+
     func testDoubleSavingToTheSameDirectoryWithoutOverwriting() async {
         // give
-        let invalidFileExtension = "json"
+        let firstFileData = "1".data(using: .utf8)!
+        let secondFileData = "2".data(using: .utf8)!
         let shouldOverwriteFile = false
         // when
         do {
-            try await package.saveInAppDirectory(
-                data: data,
+            try await fileManager.saveInAppDirectory(
+                data: firstFileData,
                 fileName: fileName,
-                fileExtension: invalidFileExtension,
+                fileExtension: fileExtension,
                 shouldOverwriteFile: shouldOverwriteFile
             )
-            
-            try await package.saveInAppDirectory(
-                data: data,
+
+            // trying to write data for the second time mustn't succeed
+            try await fileManager.saveInAppDirectory(
+                data: secondFileData,
                 fileName: fileName,
-                fileExtension: invalidFileExtension,
+                fileExtension: fileExtension,
                 shouldOverwriteFile: shouldOverwriteFile
             )
         } catch {
             // then
-            guard error as? FileManagerErrors == FileManagerErrors.fileAlreadyExists else {
-                XCTFail()
-                return
+            if let dataURL = try? fileManager.createFileURL(fileName: fileName, fileExtension: fileExtension) {
+                let fetchedData = try? Data(contentsOf: dataURL)
+                XCTAssertTrue(fetchedData == firstFileData)
+            } else {
+                XCTFail("dataURL is nil")
             }
+
+            XCTAssertEqual(error as? FileManagerError, FileManagerError.fileAlreadyExists)
         }
     }
-    
+
     func testOverwritingFilesWithTheSameName() async {
         // give
-        let invalidFileExtension = "json"
+        let firstFileData = "1".data(using: .utf8)!
+        let secondFileData = "2".data(using: .utf8)!
         let shouldOverwriteFile = true
         // when
         do {
-            try await package.saveInAppDirectory(
-                data: data,
+            try await fileManager.saveInAppDirectory(
+                data: firstFileData,
                 fileName: fileName,
-                fileExtension: invalidFileExtension,
+                fileExtension: fileExtension,
                 shouldOverwriteFile: shouldOverwriteFile
             )
-            
-            try await package.saveInAppDirectory(
-                data: data,
+
+            let firstDataURL = try fileManager.createFileURL(fileName: fileName, fileExtension: fileExtension)
+            let firstFileFetchedData = try Data(contentsOf: firstDataURL)
+
+            // overwrites the same file, must succeed
+            try await fileManager.saveInAppDirectory(
+                data: secondFileData,
                 fileName: fileName,
-                fileExtension: invalidFileExtension,
+                fileExtension: fileExtension,
                 shouldOverwriteFile: shouldOverwriteFile
             )
-            // then success
+
+            let secondDataURL = try fileManager.createFileURL(fileName: fileName, fileExtension: fileExtension)
+            let secondFetchedData = try Data(contentsOf: secondDataURL)
+
+            // first file should be overwritten
+            XCTAssertFalse(firstFileFetchedData == secondFetchedData)
         } catch {
-            print(error.localizedDescription)
-            XCTFail()
+            XCTFail(error.localizedDescription)
         }
     }
-
 }
 
-extension FileManagerSPMTests {
-    private func getAppDirectory() throws -> URL {
-        try FileManager.default.url(
-            for: .documentDirectory,
-            in: .userDomainMask,
-            appropriateFor: nil,
-            create: false
-        )
-    }
-    
+extension FPFileManagerTests {
     private func doesFileExist(with fileName: String, with fileExtension: String) throws -> Bool {
-        let appDirectory = try getAppDirectory()
-        
-        let fileURL = appDirectory
-            .appendingPathComponent(fileName)
-            .appendingPathExtension(fileExtension)
-        
+        let fileURL = try fileManager.createFileURL(fileName: fileName, fileExtension: fileExtension)
         return FileManager.default.fileExists(atPath: fileURL.path)
     }
-    
+
     private func removeFile(with fileName: String, with fileExtension: String) throws {
-        let appDirectory = try getAppDirectory()
-        
-        let fileURL = appDirectory
-            .appendingPathComponent(fileName)
-            .appendingPathExtension(fileExtension)
-        
+        let fileURL = try fileManager.createFileURL(fileName: fileName, fileExtension: fileExtension)
         try FileManager.default.removeItem(at: fileURL)
     }
 }
